9ca6951b591efd3b2fa6936bba424b3f42c6739b
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-15 11:05:27 +0200

    Fix #211

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,1 +1,10 @@
 alert('welcome to dashboard');
+
+document.addEventListener('DOMContentLoaded', function () {
+    M.Sidenav.init(
+        document.querySelectorAll('.sidenav'),
+        {
+            edge: 'right'
+        }
+    );
+});
